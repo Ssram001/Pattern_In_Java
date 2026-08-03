69294d00db074c2ae2dc1571176405b5aefe633d
Sabyasachi <krram368@gmail.com>
2026-08-03 22:06:57 +0530

    BinaryTRIANGLE

---
+++ BinaryTriangle.java
@@ -0,0 +1,23 @@
+package partten;
+
+import java.util.Scanner;
+
+public class BinaryTriangle {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Question---Binary Triangle
+//        int m = sc.nextInt();
+//        for (int i = 1; i <= m; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                if ( ((i+j)%2) ==0){
+//                    System.out.print("1");
+//                }
+//                else {
+//                    System.out.print("0");
+//                }
+//
+//            }
+//            System.out.println();
+//        }
+    }
+}
