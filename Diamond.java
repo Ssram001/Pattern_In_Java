be4d18e681fef7a3e912c30c55b45e3e4103b768
Sabyasachi <krram368@gmail.com>
2026-08-04 22:18:12 +0530

    diamond

---
+++ Diamond.java
@@ -0,0 +1,43 @@
+package partten;
+
+import java.util.Scanner;
+
+public class Diamond {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //DIAMOND
+        //METHOD-1
+//        System.out.print("Enetr the value: ");
+//        int m = sc.nextInt();
+//        int NSP = m - 1;
+//        int NST = 1;
+//        for (int i = 1; i <= m; i++) {
+//            for (int j = 1; j <= NSP; j++) {
+//                System.out.print("  ");
+//            }
+//            for (int j = 1; j <= NST; j++) {
+//                System.out.print("*" + " ");
+//            }
+//            NSP--;
+//            NST += 2;
+//            System.out.println();
+//
+//        }
+//
+//         NSP =1 ;
+//         NST = 2*m-3;
+//        for (int i = 1; i <= m; i++) {
+//            for (int j = 1; j <= NSP; j++) {
+//                System.out.print("  ");
+//            }
+//            for (int j = 1; j <= NST; j++) {
+//                System.out.print("*" + " ");
+//            }
+//            NSP++;
+//            NST -= 2;
+//            System.out.println();
+//
+//        }
+//
+    }
+}
