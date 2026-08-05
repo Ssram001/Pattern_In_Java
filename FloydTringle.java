832f426918479b52846ccad8663a9a0af926502f
Sabyasachi <krram368@gmail.com>
2026-08-05 18:55:48 +0530

    floydtriangle

---
+++ FloydTringle.java
@@ -0,0 +1,19 @@
+package partten;
+
+import java.util.Scanner;
+
+public class FloydTringle {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Question--FLOYD'S Triangle
+//        int m = sc.nextInt();
+//        int a = 1;
+//        for (int i = 1; i <= m; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print(a+" ");
+//                a++;
+//            }
+//            System.out.println();
+//        }
+    }
+}
