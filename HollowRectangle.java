b75ce8dae67e7ff63747f00c429e8c755c23c5dc
Sabyasachi <krram368@gmail.com>
2026-08-07 00:14:49 +0530

    hollowrectangle

---
+++ HollowRectangle.java
@@ -0,0 +1,24 @@
+package partten;
+
+import java.util.Scanner;
+
+public class HollowRectangle {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Question-- Hollow Rectangle
+
+//        int m =  sc.nextInt();
+//        int n = sc.nextInt();
+//        for (int i=1;i<=m;i++) {
+//            for (int j = 1; j <= n; j++) {
+//                if (i == 1|| i == m || j == 1 || j == n) {
+//                    System.out.print("* ");
+//                }
+//                else{
+//                    System.out.print("  ");
+//                }
+//            }
+//            System.out.println();
+//        }
+    }
+}
