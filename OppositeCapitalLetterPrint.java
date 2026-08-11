fd5254432f5e6281af01593eec4bc8614d77bb13
Sabyasachi <krram368@gmail.com>
2026-08-11 23:25:43 +0530

    OPPOcapitalETTER

---
+++ OppositeCapitalLetterPrint.java
@@ -0,0 +1,21 @@
+package partten;
+
+import java.util.Scanner;
+
+public class OppositeCapitalLetterPrint {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//        int n = sc.nextInt();
+//        int a=n;
+//        for (int i=1; i<=n; i++){
+//            for (int j=1; j<=a; j++){
+//
+//                System.out.print((char)(64+i));
+//            }
+//            a--;
+//            System.out.println();
+//        }
+
+    }
+}
