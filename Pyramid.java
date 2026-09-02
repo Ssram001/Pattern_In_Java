6c5c73dcb08f1a621901009ad99faeb61e897dbd
Sabyasachi <krram368@gmail.com>
2026-09-02 11:52:28 +0530

    pyramid

---
+++ Pyramid.java
@@ -0,0 +1,40 @@
+package partten;
+
+import java.util.Scanner;
+
+public class Pyramid {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//PYRAMID--
+//        METHOD--1
+
+//        int m = sc.nextInt();
+//
+//        for (int i = 1; i <= m; i++) {
+//            for (int j=1; j<=m-i;j++ ){
+//                System.out.print("  ");
+//            }
+//            for (int j=1; j<=2*i-1;j++ ){
+//                System.out.print("*"+" ");
+//            }
+//            System.out.println();
+//        }
+        ////METHOD--2
+//        int m = sc.nextInt();
+//        int NSP = m-1;
+//        int NST= 1;
+//        for (int i = 1; i <= m; i++) {
+//            for (int j=1; j<=NSP;j++ ){
+//                System.out.print("  ");
+//            }
+//            for (int j=1; j<=NST;j++ ){
+//                System.out.print("*"+" ");
+//            }
+//            NSP--;
+//            NST+=2;
+//            System.out.println();
+//        }
+
+    }
+}
