81ef9b04df62ba2f85ef662eb700481205b82ee6
Sabyasachi <krram368@gmail.com>
2026-09-04 00:26:08 +0530

    rhombos

---
+++ Rhombos.java
@@ -0,0 +1,36 @@
+package partten;
+
+import java.util.Scanner;
+
+public class Rhombos {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //RHOMBOS---
+//        int m = sc.nextInt();
+//
+//        for (int i = 1; i <= m; i++) {
+//            for (int j=1; j<=m-i;j++ ){
+//                System.out.print("  ");
+//            }
+//            for (int j=1; j<=m;j++ ){
+//                System.out.print("*"+" ");
+//            }
+//            System.out.println();
+//        }
+
+
+//        int m = sc.nextInt();
+//
+//        for (int i = 0; i < m; i++) {
+//            for (int j=1; j<=i;j++ ){
+//                System.out.print("  ");
+//            }
+//            for (int j=1; j<=m-i;j++ ){
+//                System.out.print("*"+" ");
+//            }
+//            System.out.println();
+//        }
+
+    }
+}
