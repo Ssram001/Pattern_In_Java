8785d25b5a29ca98d9841aefeceafb8eab617f3e
Sabyasachi <krram368@gmail.com>
2026-09-05 00:08:00 +0530

    starPlus

---
+++ StarPlus.java
@@ -0,0 +1,25 @@
+package partten;
+
+import java.util.Scanner;
+
+public class StarPlus {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Question--star plus
+//        int m = sc.nextInt();
+//        int n = sc.nextInt();
+//        int mid1 = (m / 2) + 1;
+//        int mid2 = (n / 2) + 1;
+//        for (int i = 1; i <= m; i++) {
+//            for (int j = 1; j <= n; j++) {
+//                if (i == mid1 || j == mid2) {
+//                    System.out.print("* ");
+//                } else {
+//                    System.out.print("  ");
+//                }
+//            }
+//            System.out.println();
+//        }
+
+    }
+}
