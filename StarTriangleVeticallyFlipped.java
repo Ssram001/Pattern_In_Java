0d65e3b742ef8323521733539a198be936e5c3e6
Sabyasachi <krram368@gmail.com>
2026-09-07 00:51:10 +0530

    StaetriVertFlipped

---
+++ StarTriangleVeticallyFlipped.java
@@ -0,0 +1,79 @@
+package partten;
+
+import java.util.Scanner;
+
+public class StarTriangleVeticallyFlipped {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Question--Star Trianle Vertically Flipped
+
+////        Method --1
+//        int m = sc.nextInt();
+//
+//        for (int i = 1; i <= m; i++) {
+//            for (int j = 1; j <=m; j++) {
+//               if (i+j>m){
+//                   System.out.print("*"+" ");
+//                }
+//               else{
+//                   System.out.print("  ");
+//               }
+//
+//            }
+//            System.out.println();
+//        }
+
+//        Method--2
+
+//        int m = sc.nextInt();
+//
+//        for (int i = 1; i <= m; i++) {
+//            for (int j=1; j<=m-i;j++ ){
+//                System.out.print("  ");
+//            }
+//            for (int j=1; j<=i;j++ ){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//Method--3
+//        int m = sc.nextInt();
+//
+//        for (int i = 1; i <= m; i++) {
+//            for (int j=1; j<=m-i;j++ ){
+//                System.out.print("  ");
+//            }
+//            for (int j=1; j<=i;j++ ){
+//                System.out.print(j+" ");
+//            }
+//            System.out.println();
+//        }
+
+
+//        int m = sc.nextInt();
+//
+//        for (int i = 1; i <= m; i++) {
+//            for (int j=1; j<=m-i;j++ ){
+//                System.out.print("  ");
+//            }
+//            for (int j=1; j<=i;j++ ){
+//                System.out.print(j+" ");
+//            }
+//            System.out.println();
+//        }
+
+
+//        int m = sc.nextInt();
+//
+//        for (int i = 1; i <= m; i++) {
+//            for (int j=1; j<=m-i;j++ ){
+//                System.out.print("  ");
+//            }
+//            for (int j=1; j<=i;j++ ){
+//                System.out.print((char)(i+64)+" ");
+//            }
+//            System.out.println();
+//        }
+
+    }
+}
