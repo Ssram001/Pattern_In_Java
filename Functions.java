ad69eec76bacfacf42bfff8cdb30ff2e9c226521
Sabyasachi <krram368@gmail.com>
2026-09-11 20:55:34 +0530

    bassics

---
+++ Functions.java
@@ -0,0 +1,58 @@
+package FUNCTION;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Functions {
+//    public static int ram(){
+//        System.out.println("I am a boy");
+//        return 4;
+//    }
+   public static void main(String[] args) {
+
+//        ram();
+//       System.out.println(ram());
+
+
+
+//MATH METHODS
+//       System.out.println(Math.sqrt(4));
+//       System.out.println(Math.cbrt(9));
+//       System.out.println(Math.abs(-1));
+//       System.out.println(Math.max(3,5));
+//       System.out.println(Math.min(8,2));
+//       System.out.println(Math.ceil(7.2));
+//       System.out.println(Math.floor(6.5));
+
+
+//       Scanner sc = new Scanner(System.in);
+//       double a = sc.nextDouble();
+//       double b = sc.nextDouble();
+//       double c = sc.nextDouble();
+//       double d = sc.nextDouble();
+//       double Z;
+//       Z = Math.max(Math.max(a,b),Math.max(c,d));
+//       System.out.println(Z);
+
+  //     child();
+
+
+
+   }
+//   public static void child(){
+//        System.out.println("I am a child");
+//        parent();
+//    }
+//
+//
+//    public static void parent(){
+//
+//        System.out.println(" We are parents");
+//        friends();
+//    }
+//
+//    public static void friends(){
+//        System.out.println("We are friends");
+//    }
+
+}
